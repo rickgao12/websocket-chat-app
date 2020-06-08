0c7357f56d31bb754360972829ced019f12f994c
dalongerlong 1 <rgao3736@gmail.com>
2020-06-07 19:17:08 -0500

    init commit

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,57 +1,52 @@
-const users = []
+const users = [];
 
-const addUser = ({
-  id,
-  username,
-  room
-}) => {
-  username = username.trim().toLowerCase();
-  room = room.trim().toLowerCase();
+const addUser = ({ id, username, room }) => {
+	if (!username || !room) {
+		return {
+			error: 'Username & room are required'
+		};
+	}
+	username = username.trim().toLowerCase();
+	room = room.trim().toLowerCase();
+	const existingUser = users.find((user) => {
+		return user.room === room && user.username === username;
+	});
 
-  if (!username || !room) {
-    return {
-      error: 'Username & room are required'
-    }
-  }
-  const existingUser = users.find((user) => {
-    return user.room === room && user.username === username
-  })
+	if (existingUser) {
+		return {
+			error: 'Username in use!'
+		};
+	}
 
-  if (existingUser) {
-    return {
-      error: 'Username in use!'
-    }
-  }
-
-  const user = {
-    id,
-    username,
-    room
-  }
-  users.push(user)
-  return {
-    user
-  }
-}
+	const user = {
+		id,
+		username,
+		room
+	};
+	users.push(user);
+	return {
+		user
+	};
+};
 
 const removeUser = (id) => {
-  const index = users.findIndex((user) => user.id === id)
-  if (index !== -1) {
-    return users.splice(index, 1)[0]
-  }
-}
+	const index = users.findIndex((user) => user.id === id);
+	if (index !== -1) {
+		return users.splice(index, 1)[0];
+	}
+};
 
 const getUser = (id) => {
-  return users.find((user) => user.id === id)
-}
+	return users.find((user) => user.id === id);
+};
 
 const getUsersInRoom = (room) => {
-  return users.filter((user) => user.room === room)
-}
+	return users.filter((user) => user.room === room);
+};
 
 module.exports = {
-  addUser,
-  removeUser,
-  getUser,
-  getUsersInRoom
-}
+	addUser,
+	removeUser,
+	getUser,
+	getUsersInRoom
+};
